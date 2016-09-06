ce7dde2ce802e2f6affdf7e783316df91ab757f7
nikmeiser <nikmeiser@NIKHILs-MacBook-Pro.local>
2016-09-06 15:40:04 -0400

    refactored flower.py

--- flower.py
+++ flower.py
@@ -5,30 +5,30 @@ import math
 
 mc = minecraft.Minecraft.create()
 pos = mc.player.getPos()
-T = minecraftturtle.MinecraftTurtle(mc,pos)
+steve = minecraftturtle.MinecraftTurtle(mc,pos)
 
-T.penblock(block.WOOL.id, 1)
-T.speed(10)
+steve.penblock(block.WOOL.id, 1)
+steve.speed(10)
 
 #Drawing shapes and flowers as specified in
 #Think Python: An Introduction to Software Design by Allen B. Downey
 
 
-def polyline(T, n, length, angle):
+def polyline(turtle, n, length, angle):
     """Draw n line segments with the given length and
     angle (in degrees) between them.
     """
     for i in range(n):
-        T.forward(length)
-        T.left(angle)
+        turtle.forward(length)
+        turtle.left(angle)
 
 
 def polygon(n, length):
     angle = 360.0/n
-    polyline(T, n, length, angle)
+    polyline(turtle, n, length, angle)
 
 
-def arc(T, r, angle):
+def arc(turtle, r, angle):
     arc_length = 2 * math.pi * r * abs(angle) / 360
     n = int(arc_length / 4) + 1
     step_length = arc_length / n
@@ -36,26 +36,26 @@ def arc(T, r, angle):
 
     # making a slight left turn before the polyline reduces
     # the error caused by the linear approximation of the arc
-    T.left(step_angle/2)
-    polyline(T, n, step_length, step_angle)
-    T.right(step_angle/2)
+    turtle.left(step_angle/2)
+    polyline(turtle, n, step_length, step_angle)
+    turtle.right(step_angle/2)
 
 
-def circle(T, r):
-    arc(T, r, 360)
+def circle(turtle, r):
+    arc(turtle, r, 360)
 
 
-def leaf(T, r, angle):
-    arc(T, r, angle)
-    T.left(180-angle)
-    arc(T, r, angle)
+def leaf(turtle, r, angle):
+    arc(turtle, r, angle)
+    turtle.left(180-angle)
+    arc(turtle, r, angle)
 
 
-def flower(T, radius, n):
+def flower(turtle, radius, n):
     r = radius/6 * n
     angle = 360/n
     for i in range(n):
-        leaf(T, r, angle)
-        T.left(angle+(180-angle))
+        leaf(turtle, r, angle)
+        turtle.left(angle+(180-angle))
 
-flower(T, 30, 5)
+flower(steve, 30, 5)
